6ca3b11a4346213c2f4c3a123caf6914b2d9b174
valandro <lvalandrorocha@hotmail.com>
2020-10-29 11:34:26 -0300

    Add resource to get one log information.

--- src/main/java/org/valandro/AccessLogController.java
+++ src/main/java/org/valandro/AccessLogController.java
@@ -1,7 +1,9 @@
 package org.valandro;
 
+import io.quarkus.vertx.web.Param;
 import io.quarkus.vertx.web.Route;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.vertx.core.http.HttpMethod;
 import org.valandro.mapper.AccessLogMapper;
 import org.valandro.repository.AccessLogRepository;
@@ -16,7 +18,13 @@ public class AccessLogController {
     @Inject
     AccessLogRepository repository;
 
-    @Route(path = "/logs", methods = HttpMethod.GET, produces = MediaType.APPLICATION_JSON)
+    @Route(path = "/log/:id", methods = HttpMethod.GET, produces = MediaType.APPLICATION_JSON)
+    public Uni<AccessLogResponse> getAccessLog(@Param("id") String id) {
+        return repository.findById(id)
+                .map(AccessLogMapper::mapTo);
+    }
+
+    @Route(path = "/log", methods = HttpMethod.GET, produces = MediaType.APPLICATION_JSON)
     public Multi<AccessLogResponse> getAccessLogs() {
         return repository.findAll()
                     .map(AccessLogMapper::mapTo);
--- src/main/java/org/valandro/repository/AccessLogRepository.java
+++ src/main/java/org/valandro/repository/AccessLogRepository.java
@@ -1,7 +1,12 @@
 package org.valandro.repository;
 
+import static org.valandro.entity.AccessLogEntity.from;
+
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.Tuple;
 import org.valandro.entity.AccessLogEntity;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -16,5 +21,11 @@ public class AccessLogRepository {
         return client.query("SELECT * FROM ACCESS_LOG").execute()
                 .onItem().transformToMulti(set -> Multi.createFrom().iterable(set))
                 .onItem().transform(AccessLogEntity::from);
+    }
+
+    public Uni<AccessLogEntity> findById(final String id) {
+        return client.preparedQuery("SELECT * FROM ACCESS_LOG WHERE ID = $1").execute(Tuple.of(Long.valueOf(id)))
+                     .onItem().transform(RowSet::iterator)
+                     .onItem().transform(iterator -> iterator.hasNext() ? from(iterator.next()) : null);
     }
 }
